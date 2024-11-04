f809b630fe0a21cf6d6a57bc1474ed385b820a2b
Jansen Choi Kai Xuan <140486215+choijans@users.noreply.github.com>
2024-11-04 13:27:20 +0800

    Update Binary Heap.c

--- Binary Heap.c
+++ Binary Heap.c
@@ -71,3 +71,91 @@ void displayHeap(minHeap heap){
     }
     printf("\n}\n");
 }
+
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define MAX 20
+
+typedef struct {
+    int data[MAX];
+    int count;
+} maxHeap;
+
+void initHeap(maxHeap *heap);
+void insertHeap(maxHeap *heap, int data);
+bool deleteHeap(maxHeap *heap);
+void displayHeap(maxHeap heap);
+
+int main(){
+    maxHeap heap;
+    initHeap(&heap);
+    
+    insertHeap(&heap, 15);
+    insertHeap(&heap, 10);
+    insertHeap(&heap, 20);
+    insertHeap(&heap, 17);
+    insertHeap(&heap, 25);
+    
+    displayHeap(heap);
+    
+    printf("\n");
+    deleteHeap(&heap);
+    displayHeap(heap);
+    
+    return 0; 
+}
+
+void initHeap(maxHeap *heap){
+    heap->count = 0;
+}
+
+void insertHeap(maxHeap *heap, int data){
+    int i;
+    for(i = heap->count; i > 0 && heap->data[(i-1)/2] < data; i = (i-1)/2){
+        heap->data[i] = heap->data[(i-1)/2];
+    }
+    
+    heap->data[i] = data;
+    heap->count++;
+}
+
+bool deleteHeap(maxHeap *heap){
+     if(heap->count == 0){
+         return false;
+     }
+     
+     heap->data[0] = heap->data[heap->count-1];
+     heap->count--;
+     
+     int i = 0;
+     while(i < heap->count){
+         int left = i*2 + 1;
+         int right = i*2 + 2;
+         int large = i; 
+         
+         if(left < heap->count && heap->data[left] > heap->data[large]){
+             large = left;
+         } else if(right < heap->count && heap->data[left] > heap->data[large]){
+             large = right; 
+         }
+         
+         if(large != i){
+             int temp = heap->data[i];
+             heap->data[i] = heap->data[large];
+             heap->data[large] = temp;
+             i = large;
+         } else {
+             break;
+         }
+     }
+}
+
+void displayHeap(maxHeap heap){
+    for(int i = 0; i < heap.count; ++i){
+        printf("%d ", heap.data[i]);
+    }
+}
+*/
